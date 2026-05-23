5a11c400a499bbee13818b1b2dd322f06fbfb0a2
NicolasDGK <nicolashermosilla56@gmail.com>
2026-05-23 03:22:47 -0400

    Update set-env.js

--- scripts/set-env.js
+++ scripts/set-env.js
@@ -1,18 +1,25 @@
 const fs = require('fs');
 const path = require('path');
 
-console.log('API_URL:', process.env.API_URL);
-console.log('API_KEY length:', process.env.API_KEY?.length);
+const apiUrl = process.env.API_URL;
+const apiKey = process.env.API_KEY;
 
-const content = `export const environment = {
-  production: true,
-  apiUrl: '${process.env.API_URL}',
-  apiKey: '${process.env.API_KEY}'
-};
-`;
+console.log('API_URL defined:', apiUrl !== undefined);
+console.log('API_KEY defined:', apiKey !== undefined);
+
+const content = [
+  'export const environment = {',
+  '  production: true,',
+  "  apiUrl: '" + apiUrl + "',",
+  "  apiKey: '" + apiKey + "'",
+  '};',
+  ''
+].join('\n');
 
 fs.writeFileSync(
   path.join(__dirname, '../src/environments/environment.prod.ts'),
   content
 );
+
 console.log('environment.prod.ts correctly generated');
+console.log('Content written:', content);
